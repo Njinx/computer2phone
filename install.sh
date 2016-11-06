783da01976b46dbc3378ea6b84b3ceb4392ec31d
Ben Allen <me@thebenallen.net>
2016-11-06 15:14:00 -0500

    readability changes

--- install.sh
+++ install.sh
@@ -7,17 +7,17 @@ echo "Installing python-magic..."
 pip install python-magic
 echo "Installing pushbullet.py..."
 pip install pushbullet.py
-function keygen {
+function kprompt {
     echo "==============================================================================================="
     echo "To use the pushbullet API we need your API key. If you aren't sure how to get your API key, visit this site: https://docs.pushbullet.com/#api-quick-start"
     echo "Please type your API key here:"
-    read key
-    echo -n $key > api-key.txt
+    read k
+    echo -n $k > api-key.txt
 }
 if [ -a api-key.txt ]; then
-    keygen
+    kprompt
 else
     touch api-key.txt
-    keygen
+    kprompt
 fi
 echo "Finished Installing!"
